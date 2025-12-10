7e375c60bc39347c65cd2c06025cea0a00f92419
bryggmastaren <charlotte.larsson01@gmail.com>
2025-12-10 02:21:29 +0100

    Updated src css

--- search.php
+++ search.php
@@ -1,20 +1,23 @@
 
 <?php get_header(); ?>
-<?php get_template_part('template-parts/header');?>
-    <div class="bg-image" style="background-image:url(<?php echo get_template_directory_uri(); ?>/assets/images/screenshot.png); height:100px; width:100%; background-size:cover; background-position:center;"> <h1>SÖKRESULTAT</h1> </div>
+
+<div class="search-container">
+    <h1>SÖKRESULTAT</h1> 
 
 
-    
+    <div class="search-content">
+    <div class="src-blog-post">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <!-- <h2>< ?php the_title(); ?> </h2> -->
 <?php the_title("<h2>","</h2>") ?>
 <?php the_excerpt("<p>","</p>") ?>
+</div>
 <?php endwhile; ?>
 
 <?php else : ?>
 <p> No posts matched this query... </p>
 <?php endif; ?>
-
-<?php get_template_part('template-parts/footer');?>
+</div>
+</div>
 <?php get_footer(); ?>
 
